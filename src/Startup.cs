ca4013feeb746acb1a648f280e0a06f2404c2337
Chris Wiederspan <chwieder@microsoft.com>
2024-06-17 14:01:06 +0000

    Updated some files

--- src/Startup.cs
+++ src/Startup.cs
@@ -9,28 +9,28 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Identity.Web;
-using WebApp_OpenIDConnect_DotNet.Services.Arm;
-using WebApp_OpenIDConnect_DotNet.Services.GraphOperations;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Identity.Web.UI;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
-namespace WebApp_OpenIDConnect_DotNet
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+using WebApp_OpenIDConnect_DotNet.Services.Arm;
+using WebApp_OpenIDConnect_DotNet.Services.GraphOperations;
+
+namespace WebApp_OpenIDConnect_DotNet {
+
+    public class Startup {
 
         public IConfiguration Configuration { get; }
 
+        public Startup(IConfiguration configuration) {
+            this.Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
+        public void ConfigureServices(IServiceCollection services) {
+
+            services.Configure<CookiePolicyOptions>(options => {
+
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
@@ -39,37 +39,38 @@ namespace WebApp_OpenIDConnect_DotNet
             });
 
             services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"))
+                .AddMicrosoftIdentityWebApp(this.Configuration.GetSection("AzureAd"))
                 .EnableTokenAcquisitionToCallDownstreamApi()
                 .AddInMemoryTokenCaches();
 
             // Add APIs
-            services.AddGraphService(Configuration);
+            services.AddGraphService(this.Configuration);
+
             services.AddHttpClient<IArmOperations, ArmApiOperationService>();
+
             services.AddHttpClient<IArmOperationsWithImplicitAuth, ArmApiOperationServiceWithImplicitAuth>()
                 .AddMicrosoftIdentityUserAuthenticationHandler(
                     "arm", 
-                    options => options.Scopes = $"{ArmApiOperationService.ArmResource}user_impersonation");
+                    options => options.Scopes = $"{ArmApiOperationService.ArmResource}user_impersonation"
+                );
 
-            services.AddControllersWithViews(options =>
-            {
+            services.AddControllersWithViews(options => {
                 var policy = new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
                     .Build();
                 options.Filters.Add(new AuthorizeFilter(policy));
             }).AddMicrosoftIdentityUI();
+
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
+            else {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
@@ -83,8 +84,8 @@ namespace WebApp_OpenIDConnect_DotNet
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
+            app.UseEndpoints(endpoints => {
+
                 endpoints.MapControllerRoute(
                     name: "Index",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
